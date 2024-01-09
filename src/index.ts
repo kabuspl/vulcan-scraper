bac8193e30dd1242ec1a67dfd78cca62bd947829
kabuspl <kabuspl@outlook.com>
2024-01-09 18:42:46 +0100

    readd index.ts containing exports of all types and main class

---
+++ src/index.ts
@@ -0,0 +1,11 @@
+export {ClassGrades, SubjectClassGrades} from "./classGrades.js";
+export {CompletedLesson} from "./completedLessons.js";
+export * from "./errors.js";
+export {Exam} from "./exams.js";
+export {Grade, Grades, SubjectGrades} from "./grades.js";
+export {Period, Register} from "./register.js";
+export {StudentGuardian, StudentInfo} from "./student.js";
+export {Subject} from "./subject.js";
+export {Timetable, TimetableLesson, TimetablePeriod} from "./timetable.js";
+
+export * from "./vulcanHandler.js";
